b97753a18fe8602b28eb527913aa50c632ec3d14
Cochetooo <chaoticalex74@gmail.com>
2024-04-11 23:04:36 +0200

    [0.2.1] feat(core/config) : Fix ProjectInfo.java not commited properly

--- musicx-desktop/src/main/java/fr/xahla/musicx/desktop/DesktopApplication.java
+++ musicx-desktop/src/main/java/fr/xahla/musicx/desktop/DesktopApplication.java
@@ -11,7 +11,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
