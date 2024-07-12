79c3d7735aab7839e49a4c26639d9316c52130e1
Alexis Cochet <alexis.cochet@kis-external.fr>
2024-07-12 16:45:59 +0200

    [0.6.0] First Commit as C# project

---
+++ Musicx/Views/Menu/TopBarMenu.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Musicx.Views.Menu;
+
+public partial class TopBarMenu : UserControl
+{
+    public TopBarMenu()
+    {
+        InitializeComponent();
+    }
+}
