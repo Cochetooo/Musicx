7de36565e472aa08677e7c6eefb8d646f5ede117
Alexis Cochet <alexis.cochet@kis-external.fr>
2024-07-16 16:51:04 +0200

    [0.6.0] Added Logo + AudioPlayer controls Front End

--- Musicx/MainWindow.xaml.cs
+++ Musicx/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,16 +9,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
 namespace Musicx;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
+    private static readonly log4net.ILog log =
+        log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    
     public MainWindow()
     {
+        log.Info("Initializing program...");
+        var stopwatch = new Stopwatch();
+        
+        stopwatch.Start();
+        
         InitializeComponent();
+        
+        stopwatch.Stop();
+        log.Info("Program initialized in " + stopwatch.ElapsedMilliseconds + "ms.");
     }
 }
---
+++ Musicx/Views/Bottom/AudioPlayer.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Musicx.Views.Bottom;
+
+public partial class AudioPlayer : UserControl
+{
+    public AudioPlayer()
+    {
+        InitializeComponent();
+    }
+}
