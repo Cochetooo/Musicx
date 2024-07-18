b1d93dd7b038a96051a8e06932d0a32c760b1b06
Cochetooo <chaoticalex74@gmail.com>
2024-07-18 23:43:44 +0200

    [0.6.0] Improve UI for Module Selector

--- Musicx/MainWindow.xaml.cs
+++ Musicx/MainWindow.xaml.cs
@@ -25,7 +25,6 @@ public partial class MainWindow
     {
         Log.Info("Initializing program...");
         var stopwatch = new Stopwatch();
-        
         stopwatch.Start();
         
         InitializeComponent();
--- Musicx/Views/AudioPlayer/AudioPlayer.xaml.cs
+++ Musicx/Views/AudioPlayer/AudioPlayer.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System.Windows.Controls;
+using log4net;
 
 namespace Musicx.Views.AudioPlayer;
 
-public partial class AudioPlayer : UserControl
+public partial class AudioPlayer
 {
+    private static readonly ILog Log = LogManager.GetLogger(typeof(AudioPlayer));
+    
     public AudioPlayer()
     {
         InitializeComponent();
