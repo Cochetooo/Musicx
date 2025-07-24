ba584e75427fefbbf16b30e1c4620f9c93dd45c7
Cochetooo <chaoticalex74@gmail.com>
2025-07-25 00:30:10 +0200

    [0.6.2] feat(Pgsql) Added fixtures

---
+++ Fixtures/Genres.sql
@@ -0,0 +1,1 @@
+INSERT INTO public."Genres" ("Id", "CreatedAt", "UpdatedAt", "Name", "Description", "Color") VALUES (1, '2025-07-24 23:58:05.572049', '2025-07-24 23:58:05.572191', 'Metal', null, '#F6B26B');
