cc59d26846de143c44c3a6940925840a7f22e4cf
Alexis Cochet <alexis.cochet@kis-external.fr>
2025-09-29 17:05:37 +0200

    [0.6.7] feat(Web Front) Started album track genre edit modal

---
+++ Musicx.Presentation.Web/Musicx.Presentation.Web.Client/Modals/Admin/Albums/AlbumTrackGenresEditModal.razor.cs
@@ -0,0 +1,30 @@
+﻿using MudBlazor;
+using Musicx.Contracts.Dto.Requests;
+using Musicx.Contracts.Dto.Responses;
+
+namespace Musicx.Presentation.Web.Client.Modals.Admin.Albums;
+
+public partial class AlbumTrackGenresEditModal
+{
+    private MudDialog _modalRef = null!;
+    
+    private List<OutGenre> _genres = [];
+    
+    private List<InSong> _albumSongs = [];
+    private InSong? _selectedSong;
+
+    private List<OutGenre> _selectedPrimaryGenres = [];
+    private List<OutGenre> _selectedInfluenceGenres = [];
+
+    private void AddPrimaryGenre(OutGenre genre)
+        => _selectedPrimaryGenres.Add(genre);
+
+    private void RemovePrimaryGenre(OutGenre genre)
+        => _selectedPrimaryGenres.Remove(genre);
+    
+    private void AddInfluenceGenre(OutGenre genre)
+        => _selectedInfluenceGenres.Add(genre);
+    
+    private void RemoveInfluenceGenre(OutGenre genre)
+        => _selectedInfluenceGenres.Remove(genre);
+}
