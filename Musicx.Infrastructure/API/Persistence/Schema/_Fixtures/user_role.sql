5093866d2542da82b969b9abf2986db5e7df669c
Cochetooo <chaoticalex74@gmail.com>
2025-10-05 11:42:17 +0200

    [0.6.7] feat(SQL) Updated fixtures to 04-10-2025

--- Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/user_role.sql
+++ Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/user_role.sql
@@ -17,3 +17,6 @@ INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (16, 
 INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (17, 1);
 INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (18, 1);
 INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (19, 1);
+INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (20, 1);
+INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (21, 1);
+INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (22, 1);
