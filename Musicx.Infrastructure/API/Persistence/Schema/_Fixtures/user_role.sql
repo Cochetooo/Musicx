9ab882eded376b8a5418f96e100f3ecc4414a548
Alexis Cochet <alexis.cochet@kis-external.fr>
2025-10-29 13:30:04 +0100

    [0.6.8] feat(SQL) Updated fixtures to 29-10-25 work

--- Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/user_role.sql
+++ Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/user_role.sql
@@ -29,3 +29,4 @@ INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (28, 
 INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (29, 1);
 INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (30, 1);
 INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (31, 1);
+INSERT INTO public.user_role (user_role_user_id, user_role_role_id) VALUES (32, 1);
