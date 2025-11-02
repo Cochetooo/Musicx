34793ca3a7ef7236ad2fa4b36a4f5d099a6f5a28
Cochetooo <chaoticalex74@gmail.com>
2025-11-03 00:33:11 +0100

    [0.6.7] feat(Web Front) Improved some UI

--- Musicx.Contracts/Enums/CollectionType.cs
+++ Musicx.Contracts/Enums/CollectionType.cs
@@ -1,14 +1,31 @@
-﻿namespace Musicx.Contracts.Enums;
+﻿using System.ComponentModel;
+
+namespace Musicx.Contracts.Enums;
 
 [Flags]
 public enum CollectionType
 {
+    [Description("💻 Digital")]
     Digital             = 1 << 0,
+    
+    [Description("💿 CD")]
     Cd                  = 1 << 1,
+    
+    [Description("📀 DVD")]
     Dvd                 = 1 << 2,
+    
+    [Description("💽 Vinyl")]
     Vinyl               = 1 << 3,
+    
+    [Description("📼 Cassette")]
     Cassette            = 1 << 4,
+    
+    [Description("❓ Other")]
     Other               = 1 << 5,
+    
+    [Description("👀 Wishlist")]
     Wishlist            = 1 << 6,
+    
+    [Description("⛓️‍💥 Used to Own")]
     UsedToOwn           = 1 << 7,
 }
