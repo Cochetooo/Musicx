ebfa18d45239e7fc25e4a5b69cf4b88297123f4d
Alexis Cochet <alexis.cochet@kis-external.fr>
2025-11-03 16:51:55 +0100

    [0.6.8] feat(SQL) Updated fixtures to 03-11-2025 work

--- Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/permissions.sql
+++ Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/permissions.sql
@@ -1,1 +1,2 @@
 INSERT INTO public.permissions (permission_id, permission_created_at, permission_updated_at, permission_name) VALUES (1, '2025-09-23 15:08:34.000000', '2025-09-23 15:08:36.000000', 'album.rate');
+INSERT INTO public.permissions (permission_id, permission_created_at, permission_updated_at, permission_name) VALUES (2, '2025-11-03 14:37:08.000000', '2025-11-03 14:37:10.000000', 'user.album.attrs.save');
--- Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/role_permission.sql
+++ Musicx.Infrastructure/API/Persistence/Schema/_Fixtures/role_permission.sql
@@ -1,1 +1,2 @@
 INSERT INTO public.role_permission (role_permission_role_id, role_permission_permission_id) VALUES (2, 1);
+INSERT INTO public.role_permission (role_permission_role_id, role_permission_permission_id) VALUES (2, 2);
